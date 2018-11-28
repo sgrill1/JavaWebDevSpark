b83d83a92175457e0f042ae46bdb57ba3cedb5e7
Sam Grill <samgrill@Sams-MacBook-Pro.local>
2018-11-28 15:36:08 +0000

    added a css file to change font

--- src/main/java/com/sdg/spark/App.java
+++ src/main/java/com/sdg/spark/App.java
@@ -10,10 +10,12 @@ import java.util.Map;
 
 import static spark.Spark.get;
 import static spark.Spark.post;
+import static spark.Spark.staticFileLocation;
 
 public class App
 {
     public static void main( String[] args ) {
+        staticFileLocation("/public");
         CourseIdeaDAO dao = new SimpleCourseIdeaDAO(); //PROTOTYPE: WOULD USUALLY USE A DATABASE
 
 
