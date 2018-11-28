436cad3816777a7b2a5ace1071ca11cecd3365cf
Sam Grill <samgrill@Sams-MacBook-Pro.local>
2018-11-28 13:47:41 +0000

    add a dynamic sign in page using query parameters

--- src/main/java/com/sdg/spark/App.java
+++ src/main/java/com/sdg/spark/App.java
@@ -3,7 +3,11 @@ package com.sdg.spark;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class App
 {
@@ -11,6 +15,12 @@ public class App
 
         get("/", (req,res) -> {
             return new ModelAndView(null, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/sign-in", (req,res) -> {
+            Map<String, String> model = new HashMap<>();
+            model.put("username", req.queryParams("username"));
+            return new ModelAndView(model, "sign-in.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
